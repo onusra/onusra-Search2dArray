7273d1a1e60ee5f0893ef719348a0b91763025e9
onusra <onusra@angelo.edu>
2023-07-12 21:08:01 +0000

    Completed

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -12,6 +12,8 @@ public class Search2dArray {
     public static void main(String [] m){
         
     }
+
+    
     public boolean search(int[][] nums, int target){
         for(int i = 0; i < nums.length; i++){
             for(int j = 0; j < nums.length; j++){
